342579535daa1c31e040a1530f4cd912102c5e72
tareef <tareef@fastmail.com>
2018-07-27 17:28:22 +0300

    add react

---
+++ src/client/index.jsx
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+import { APP_CONTAINER_SELECTOR } from '../shared/config';
+
+ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR));
--- src/server/index.js
+++ src/server/index.js
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
 app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)'
-    : '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`);
+    : '(development).\nKeep "npm run dev:wds" running in an other terminal'}.`);
 });
