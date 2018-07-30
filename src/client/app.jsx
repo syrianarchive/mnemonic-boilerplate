a4c18c3cc287c98d2df8fd820f460584136e988a
tareef <tareef@fastmail.com>
2018-07-30 21:56:36 +0300

    Redux, Immutable, and Fetch

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import HelloButton from './container/hello-button';
+import HelloAsyncButton from './container/hello-async-button';
+import Message from './container/message';
+import MessageAsync from './container/message-async';
+import { APP_NAME } from '../shared/config';
+
 
 const App = () => (
-  <h1>
-Hello React!! Tareef
-  </h1>
+  <div>
+    <h1>
+      {APP_NAME}
+    </h1>
+    <Message />
+    <HelloButton />
+    <MessageAsync />
+    <HelloAsyncButton />
+  </div>
 );
 
 export default App;
